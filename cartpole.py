a8622e92668722799e908d6430a76d9ed962a90b
Robert Stewart <rstewart@computer.org>
2017-10-21 15:07:22 -0600

    First hack at balancing. Works better than nothing.

--- cartpole.py
+++ cartpole.py
@@ -1,12 +1,33 @@
 import gym
+import time
+
 env = gym.make('CartPole-v0')
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+LEFT = 0
+RIGHT = 1
+
+
+def choose_direction(observation):
+    radians = observation[2]
+    if radians < 0:
+        return LEFT
+    else:
+        return RIGHT
+    #return env.action_space.sample()
+
+
+def main():
+    for i_episode in range(1):
+        observation = env.reset()
+        for t in range(500):
+            env.render()
+            #time.sleep(.5)
+            print(observation)
+            action = choose_direction(observation)
+            observation, reward, done, info = env.step(action)
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
+
+
+if __name__ == "__main__":
+    main()
